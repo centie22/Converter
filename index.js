4661529065cd15fd92292da007d9b022ca3a681c
centie22 <98043071+centie22@users.noreply.github.com>
2022-06-20 14:58:27 +0100

    Converter

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hi!!!!")
+
+const Input= document.getElementById("input");
+const Result= document.getElementById("result");
+const inputType= document.getElementById("inputType");
+const resultType= document.getElementById("resultType");
